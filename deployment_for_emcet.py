7c1ea11f933a303c3af0fc00831b45c170707b5d
sravya520 <112112779+sravya520@users.noreply.github.com>
2024-07-14 16:20:39 +0530

    Update deployment_for_emcet.py

--- deployment_for_emcet.py
+++ deployment_for_emcet.py
@@ -49,6 +49,7 @@ def predict_colleges(input_rank, input_gender, input_caste):
 
 # Streamlit UI
 st.title('College Recommendation System')
+st.markdown("By Dangeti Sravya")
 
 # Input form
 input_rank = st.number_input('Enter your rank:', min_value=1, max_value=10000000, step=1)
